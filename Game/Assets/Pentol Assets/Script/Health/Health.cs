a348234ac803208e6e13354ed646b01ec6d14669
Ferdian <fergoddota2@gmail.com>
2021-11-29 20:38:48 +0700

    trap
    trap

--- Game/Assets/Pentol Assets/Script/Health/Health.cs
+++ Game/Assets/Pentol Assets/Script/Health/Health.cs
@@ -4,12 +4,13 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
-
+    private Animator anim;
+    private bool dead;
 
     private void Awake()
     {
         currentHealth = startingHealth;
-     
+        anim = GetComponent<Animator>();
     }
     public void TakeDamage(float _damage)
     {
@@ -17,19 +18,18 @@ public class Health : MonoBehaviour
 
         if (currentHealth > 0)
         {
-
-            //player hurt
+            anim.SetTrigger("hurt");
+            //iframes
         }
         else
         {
-            //player dead
-
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+                GetComponent<Movement>().enabled = false;
+                dead = true;
+            }
         }
-    }
-
-    public void Update(){
-        if(Input.GetKeyDown(KeyCode.E))
-        TakeDamage(1);
     }
     public void AddHealth(float _value)
     {
